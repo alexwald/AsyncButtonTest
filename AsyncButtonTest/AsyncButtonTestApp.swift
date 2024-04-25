f5648ebdc1a84182a746e056289a3417dd9fb24e
Alexander Wald <wald.alexander@gmail.com>
2024-04-25 08:37:01 +0200

    cleanup

--- AsyncButtonTest/AsyncButtonTestApp.swift
+++ AsyncButtonTest/AsyncButtonTestApp.swift
@@ -6,21 +6,11 @@
 //
 
 import SwiftUI
-import ContextSDK
 
 @main
 struct AsyncButtonTestApp: App {
-    
-    init() {
-        ContextManager.setup("1eb9f38ef755da3aa45a5b2717639421716f4b2bf2b6a6685fbfad8fc6ea5cfd71642b647c85a317f0461cce334a1c891c049c05f1bdfa0791f512b35f11c6f0")
-print("App init")
-    }
-    
-//    ContextManager.setup("1eb9f38ef755da3aa45a5b2717639421716f4b2bf2b6a6685fbfad8fc6ea5cfd71642b647c85a317f0461cce334a1c891c049c05f1bdfa0791f512b35f11c6f0")
-    
     var body: some Scene {
         WindowGroup {
-//            ContextManager.setup("1eb9f38ef755da3aa45a5b2717639421716f4b2bf2b6a6685fbfad8fc6ea5cfd71642b647c85a317f0461cce334a1c891c049c05f1bdfa0791f512b35f11c6f0")
             return ContentView()
         }
     }
